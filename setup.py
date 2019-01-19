257224ddd6802bd79aa303dd02ae7ef09ea2772b
Sergey Mkrtchyan <sergey.mkrtchyan@gmail.com>
2019-01-19 14:08:50 -0500

    Add setup.py and LICENSE

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="word2vec_np",
+    version="0.0.1",
+    author="Sergey Mkrtchyan",
+    author_email="sergey.mkrtchyan@gmail.com",
+    description="word2vec CBOW model implementation in numpy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mkserge/word2vec_np",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
